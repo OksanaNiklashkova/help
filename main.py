e95e0da0fbe121a0766cee608487e042895f82b9
Oksana Niklashkova <o.niklashkova@gmail.com>
2025-03-23 12:28:16 +0300

    new functions

--- main.py
+++ main.py
@@ -20,3 +20,17 @@ print(
     f"""Входные данные данные - {input_date}
 Результат обработки - {get_date(input_date)}"""
 )
+
+# функция, маскирующая данные о счете или карте
+common_for_mask = input_information[int(input("Введите номер строки от 1 до 8: "))]
+print(
+    f"""Входные данные данные - {common_for_mask}
+Результат обработки - {mask_account_card(common_for_mask)}"""
+)
+
+# функция, преобразующая дату в стандартный формат
+input_date = str(input_information[int(input("Введите номер строки от 10 до 12: "))])
+print(
+    f"""Входные данные данные - {input_date}
+Результат обработки - {get_date(input_date, flow=True)}"""
+)
